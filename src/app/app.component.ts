8bb082bc6000e2cb66eea4bfd1fbd2d5cdfaae92
Burak <isikburak142@gmail.com>
2021-09-23 10:13:42 +0200

    Only first two functions are implemented.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  images = ["./assets/auto.jpg", "./assets/motor.jpg", "./assets/scooter.jpg"]
+  ngOnInit(): void { }
+
+  selectedImageURL: string = '';
+  selectedVehicles: string[] = [];
+  selectChangeHandler(event: any) {
+    console.log(event.target.value);
+    if (event.target.value === "auto") {
+      this.selectedVehicles = ["Hatchback", "Sedan", "Station", "Cabriolet", "Coupé", "MPV", "Terreinauto"]
+      this.selectedImageURL = this.images[0];
+      return this.selectedImageURL
+    } else if (event.target.value === "motor") {
+      this.selectedVehicles = ["All-road", "Naked", "Enduro", "Race", "Toermotor", "Chopper", "Zijspan"];
+      this.selectedImageURL = this.images[1];
+      return this.selectedImageURL
+    }
+    else {
+      this.selectedVehicles = [];
+      this.selectedImageURL = this.images[2];
+      return this.images[2]
+    }
+
+
+  }
+
+
 }
+
